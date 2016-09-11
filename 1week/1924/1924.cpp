1585cfd5f539c1e93c37097a344a824edc6ca125
hobbes <hobbes@WINDOW>
2016-09-11 13:37:11 +0900

    앙 풀었띠~!

--- 1week/1924/1924.cpp
+++ 1week/1924/1924.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
-	printf("HELL WORLD");
+	int month;
+	int day;
+	int pastDays = 0;
+	int monthData[12][2] = { {1,31},{2,28},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},{9,30},{10,31},{11,30},{12,31} };//recording Data of each month's day
+	char * week[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+	scanf("%d %d", &month, &day);
+
+	for (int i = 0; i < month-1; i++)
+	{
+		pastDays = pastDays + monthData[i][1];//adding sigma(month-1) as pastDays
+	}
+
+	pastDays = pastDays + day; //adding day
+	printf("%s", week[pastDays % 7]);//if pastDay % 7 == 0, Sunday 
+
 	return 0;
 }
